55ffa355dc2803d1c9457d7d55e1b031211f87e2
chasehaye <chasehayejoc@gmail.com>
2026-04-24 18:17:49 -0700

    actions

--- internal/config/enforceENV.go
+++ internal/config/enforceENV.go
@@ -21,6 +21,7 @@ func CheckRequiredEnvVars() {
 		"SENDER_ADDRESS",
 		"PORT",
 	}
+	
 	for _, v := range required {
 		if os.Getenv(v) == "" {
 			log.Fatalf("CRITICAL CONFIG ERROR: Environment variable '%s' is missing. Server cannot start.", v)
