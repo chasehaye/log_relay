8c50060812ffa3b8beffbe706d3d82cfac20dd81
chasehaye <chasehayejoc@gmail.com>
2026-04-24 20:22:36 -0700

    actions

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
+
 set -e
 
-echo "Building..."
+cd /srv/webserver-back/fudesoftware/
+
+echo "Building the application..."
 go build -o ./build/main ./cmd/server/main.go
 
 echo "Stopping old process..."
 pkill main || true
 
 echo "Starting new process..."
-nohup ./build/main > app.log 2>&1 &
+
+nohup ./build/main > ./build/server.log 2>&1 &
+
+echo "Server started. Logs at ./build/server.log"
