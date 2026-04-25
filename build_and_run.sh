c30773a0a9eed4f8363e549835a6f352379fd007
chasehaye <chasehayejoc@gmail.com>
2026-04-24 20:15:43 -0700

    actions

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-echo "Go environment status:" && \
-go env && \
-GOMODCACHE=`go env GOMODCACHE` && \
-echo "Go mod cache directory:" && \
-echo $GOMODCACHE && \
-ls -lsa $GOMODCACHE && \
-echo "Building the application..." && \
-go build -o ./build/main ./cmd/server/main.go && \
-echo "Starting the application..." && \
-./build/main
+set -e
+
+echo "Go environment status:"
+go env
+
+echo "Building the application..."
+go build -o ./build/main ./cmd/server/main.go
+
+echo "Starting the application..."
+exec ./build/main
